3c3119b989177ce1ef1dd31b102f1a7ac16ec496
dominait <jim@jimhortons.com>
2021-03-16 09:50:40 -0600

    react a thinggy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
- import IconButton from './components/buttons/IconButton'
- import AppBar from './dashboard/appbar'
+
 function App() {
   return (
     <div className="App">
-      <AppBar inner={<IconButton/>} title="james foley">
-  
-         
-      </AppBar>
+      Hello
     </div>
   );
 }
